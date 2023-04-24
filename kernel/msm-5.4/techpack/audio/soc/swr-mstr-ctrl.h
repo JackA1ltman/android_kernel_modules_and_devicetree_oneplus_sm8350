d64962f282b5480e032101ba767efe6076ae67b0
Soumya Managoli <quic_c_smanag@quicinc.com>
2023-04-24 12:08:53 +0530

    soc: swr-mstr-ctrl: add new lock to sync runtime_resume and runtime_suspend
    runtime_suspend may get from multi functions and reslock is
    unlock and lock inbetween which may cause clk count mismatch
    with multi rutime_suspend called. Add new lock to make sure
    runtime_suspend is run in sequence.
    
    Change-Id: Ie465ded6dc1db1244035e9f4d216466b630df82b
    Signed-off-by: Meng Wang <mengw@codeaurora.org>
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>
    Signed-off-by: Chippa-a <bagirovvusal99@gmail.com>

--- kernel/msm-5.4/techpack/audio/soc/swr-mstr-ctrl.h
+++ kernel/msm-5.4/techpack/audio/soc/swr-mstr-ctrl.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _SWR_WCD_CTRL_H
@@ -134,10 +135,7 @@ struct swr_mstr_ctrl {
 	struct mutex reslock;
 	struct mutex pm_lock;
 	struct mutex irq_lock;
-#ifdef OPLUS_BUG_STABILITY
-	/* Apply CR#3096189 to swr-mstr-ctrl: add new lock to sync runtime_resume and runtime_suspend */
 	struct mutex runtime_lock;
-#endif
 	u32 swrm_base_reg;
 	char __iomem *swrm_dig_base;
 	char __iomem *swrm_hctl_reg;
