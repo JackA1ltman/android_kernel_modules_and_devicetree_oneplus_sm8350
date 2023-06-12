f46997dca097d4d54af9f620fc66ef4c17b87e70
illa lakshmi soujanya <quic_illa@quicinc.com>
2023-06-12 15:09:32 +0530

    msm: camera: sensor: Add changes to prevent unmap buffers
    The function cam_mem_mgr_release can unmap buffers when in use.
    This change with cam_mem_put_cpu_buf prevents unmaping the buffers in use.
    
    CRs-Fixed: 3489559
    Change-Id: I9c4e284c5961a2eb4ff0df362c93d6cea7d77cab
    Signed-off-by: illa lakshmi soujanya <quic_illa@quicinc.com>

--- kernel/msm-5.4/techpack/camera/drivers/cam_sensor_module/cam_flash/cam_flash_core.c
+++ kernel/msm-5.4/techpack/camera/drivers/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -1159,6 +1160,7 @@ int cam_flash_i2c_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 
 				break;
 			}
+			cam_mem_put_cpu_buf(cmd_desc[i].mem_handle);
 		}
 		power_info = &fctrl->power_info;
 		if (!power_info) {
@@ -1323,6 +1325,7 @@ update_req_mgr:
 				add_req.req_id, add_req.trigger_eof);
 		}
 	}
+	cam_mem_put_cpu_buf(config.packet_handle);
 	return rc;
 }
 
@@ -1499,6 +1502,8 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 			rc = -EINVAL;
 			return rc;
 		}
+
+		cam_mem_put_cpu_buf(cmd_desc->mem_handle);
 		break;
 	}
 	case CAM_FLASH_PACKET_OPCODE_SET_OPS: {
@@ -1606,6 +1611,8 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 			rc = -EINVAL;
 			return rc;
 		}
+
+		cam_mem_put_cpu_buf(cmd_desc->mem_handle);
 		break;
 	}
 	case CAM_FLASH_PACKET_OPCODE_NON_REALTIME_SET_OPS: {
@@ -1748,6 +1755,7 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 			return rc;
 		}
 
+		cam_mem_put_cpu_buf(cmd_desc->mem_handle);
 		break;
 	}
 	case CAM_PKT_NOP_OPCODE: {
@@ -1810,6 +1818,7 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 		}
 	}
 
+	cam_mem_put_cpu_buf(config.packet_handle);
 	return rc;
 }
 
