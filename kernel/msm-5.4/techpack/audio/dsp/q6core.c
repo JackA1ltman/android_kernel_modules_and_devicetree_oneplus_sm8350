cb63fe73ba39864dadd52dbeaf5c68805869b20e
Soumya Managoli <quic_c_smanag@quicinc.com>
2023-06-23 12:57:51 +0530

    ASoC: dsp: q6core: Avoid use after free
    Add check for AVCS_CMD_RSP_LOAD_MODULE response payload
    to avoid its access after free.
    
    Change-Id: I3023e6676a27fe33d2cc0f44a49813f0ed0ebe3b
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>
    Signed-off-by: Chippa-a <bagirovvusal99@gmail.com>

--- kernel/msm-5.4/techpack/audio/dsp/q6core.c
+++ kernel/msm-5.4/techpack/audio/dsp/q6core.c
@@ -475,12 +475,8 @@ static int32_t aprv2_core_fn_q(struct apr_client_data *data, void *priv)
 	case AVCS_CMD_RSP_LOAD_MODULES:
 		pr_debug("%s: Received AVCS_CMD_RSP_LOAD_MODULES\n",
 			 __func__);
-#ifdef OPLUS_ARCH_EXTENDS
-		/* Apply CR#3531543 to avoid use after free */
 		if (!rsp_payload)
 			return -EINVAL;
-#endif /*OPLUS_ARCH_EXTENDS*/
-
 		if (data->payload_size != ((sizeof(struct avcs_load_unload_modules_sec_payload)
 			* rsp_payload->num_modules) + sizeof(uint32_t))) {
 			pr_err("%s: payload size greater than expected size %d\n",
@@ -1105,10 +1101,7 @@ int32_t q6core_avcs_load_unload_modules(struct avcs_load_unload_modules_payload
 done:
 	kfree(mod);
 	kfree(rsp_payload);
-#ifdef OPLUS_ARCH_EXTENDS
-	/* Apply CR#3531543 to avoid use after free */
 	rsp_payload = NULL;
-#endif /*OPLUS_ARCH_EXTENDS*/
 	mutex_unlock(&(q6core_lcl.cmd_lock));
 	return ret;
 }
